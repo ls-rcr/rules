2d5cbbe9e8536d78cc7edbc1abfde242f762fcea
Sondre Kjempekjenn <sondre.kjempekjenn@gmail.com>
2020-12-06 17:33:01 +0100

    Add english

---
+++ rules_english.ts
@@ -0,0 +1,149 @@
+export const RULES_ENGLISH = `
+
+# Lsrcr Rules
+
+1. # Rules of *All* Lsrcr Services
+   1. **You or your account(s) shall not--intentionally or unintentionally--do any of the following actions**:
+      1. **Allow another person to access any of your Lsrcr-related accounts** including external accounts used to access or attached to Lsrcr services.
+      2. **Send or share any of the following content**:
+         1. **Malicious files (malware)**: content including, but not limited to, trojans, ransomware, keyloggers, DDoS, or game cheats.
+         2. **Executable files**: content including, but not limited to, ".EXE" files, scripts, or any other file designed to be run or executed on any system.
+         3. **Mature content**: content including, but not limited to, pornography, realistic gore or violence, nudity, and sexual content.
+         4. **Copyrighted content**: content including, but not limited to, any content that the owner holds the exclusive rights to that you do not have permission to distribute.
+         5. **Spam**: content including, but not limited to:
+            1. Advertisements for non-Lsrcr servers or projects.
+            2. Junk.
+            3. Repetitive content or messages.
+            4. Support tickets or forum posts (like reports or unban appeals) sent to staff in an attempt to expedite its review.
+            5. Excessive cursing/swearing.
+            6. Asking for, begging for, or otherwise contacting anyone for the purpose of obtaining any donation item, in-game items, in-game cash, or any other service or entity unless that person has an open advertisment on the Lsrcr forum explicitly stating their intention to sell or give away the item in question.
+         6. **Harassing content**: content including, but not limited to, threats to commit real-life crime or violation of these rules, repetitive insults, racial slurs, bullying, asking for personal details, content containing explicit or implicit sexual overtones, or any other content sent to an individual after being asked by that person to stop sending such content.
+         7. **Controversial content**: content including, but not limited to, religions, deities, cults, politics, and world events.
+         8. **Any content pertaining to using, obtaining, creating, or otherwise related to rule 1.1.2.1 or 1.1.2.2**.
+         9. **Any content that encourages others to break these rules**.
+      3. **Abuse any bug, glitch, error, flaw, fault, or any other unintended function**.
+         1. Any such bug, glitch, error, fault, or other unintended function shall be immediately reported to a Lsrcr staff member and shall not be revealed to anyone but Lsrcr staff.
+      4. **Impersonate another**.
+      5. **Lie to an administrator during an investigation of rule-break**.
+      6. **Disobey orders from a staff member**. If you feel a staff member's order is unreasonable, collect evidence and report that staff member to a higher-ranked moderator or administrator. If a staff member is telling you to do something, there is likely a good reason for it.
+      7. **Copy, steal, recreate, reverse-engineer, or otherwise take any Lsrcr created, operated, or used content**. This includes, but is not limited to the game script, forum content, groups, etc.
+      8. **Research, seek out, or disseminate any private or identifying information about any individual or organization other than yourself**. This includes, but is not limited to images, real names, email addresses, phone numbers, physical addresses, etc. This is commonly known as "Doxing".
+      9. **Access any account, system, profile, etc. that does not belong to you, or that you are not granted access to by an administrator**. This includes committing or attempting to commit any sort of cyber attack against Lsrcr or its players.
+   2. EXCEPTIONS to the "Lsrcr Services" rule:
+      1. Content sent through non-Lsrcr services (e.g. Discord PMs) are not policed by Lsrcr staff unless all of the following are met:
+         1.  A user contacts another user with the intent to violate rule 1.1 without the other user's consent.
+         2. The user was able to contact the other user <u>solely</u> as a result of the two users being a part of Lsrcr's Discord.
+
+2. # Lsrcr SAMP-Specific Rules
+
+   1. **You or your account(s) shall not--intentionally or unintentionally--do any of the following actions**:
+      1. **Deathmatch**. Deathmatch includes:
+         1. Shooting or otherwise causing damage to innocent players as an innocent or law enforcement officer.
+         2. Shooting or otherwise causing damage to another player who you <u>successfully</u> robbed, raped, broke, etc. before they attacked you first.
+         3. Shooting or otherwise causing damage to another player who only <u>unsuccessfully</u> robbed, raped, broke, etc. you.
+         4. Carrying out your own hit.
+         5. Assisting another kill someone where you had no justification to attack.
+         6. EXCEPTIONS to rule 2.1.1.1 through 2.1.1.5:
+            1. A hitman may kill any player who has a hit.
+            2. Shooting a vehicle where the driver is innocent, but you are justified in shooting the passenger(s) is allowed.
+            3. Utilizing melee weapons, excluding chainsaw, you may attack any person for any reason. Vehicles are NOT considered melee weapons for the purposes of this exception. This exception does not allow violating harassment rules (rule 2.1.2).
+         7. EXCEPTIONS to rule 2.1.1.4:
+            1. Pagati members may carry out their own hits in the course of proper Pagati roleplay.
+      2. **Harass others**. Harassment includes:
+         1. Committing any of the following after being asked by the other player to stop:
+            1. Following someone for more than three (3) minutes within a ten (10) minute period unless attacking as allowed by rule 2.1.1 (excluding rule 2.1.1.6.3).
+            2. Committing more than three (3) hostile actions toward another player within a ten (10) minute period if that player did not retaliate with an attack or a hostile action in return. A "hostile action" means anything from raping, robbing, or breaking to killing as allowed by rule 2.1.1.
+         2. EXCEPTIONS to rule 2.1.2.1:
+            1. The following classes are exempt under the following circumstances:
+               1. Law enforcement - when chasing a wanted player
+               2. Hitmen - when chasing a player with a hit
+               3. Pagati - in the course of proper Pagati roleplay or when chasing a player with a hit
+      3. **Abuse commands**. Command abusing includes:
+         1. Using /rp without a valid reason. Valid reasons include, but are not limited to:
+            1. Holding a weapon in public
+            2. Assault
+            3. Reckless driving
+            4. Damaging property
+         2. Using /tune to avoid damage to your vehicle for any reason.
+      4. **Cheat**. Cheating includes:
+         1. Using any modification or tool that may give you an unfair advantage over another player who does not use the tool or modification. This includes, but is not limited to any of the following:
+            1. Aimbot
+            2. Airbrake cheat
+            3. Key-binds
+            4. Teleport cheat
+         2. Failing to report a cheater.
+         3. Using the BMX high-jump SMG bug to escape a player allowed to kill you per rule 2.1.1.
+         4. Entering the menu (Esc) or minimizing the game to evade a player allowed to kill you per rule 2.1.1.
+         5. Warping through solid objects to evade a player allowed to kill you or to access an otherwise inaccessible area by entering the passenger seat of vehicles ("G" abuse).
+         6. Refilling your armor more than once from weapon checkpoints in the middle of a fight.
+      5. **Place random hits**. Valid hit reasons include hitting a player within five (5) minutes of any of the following actions being taken:
+         1. For successfully or unsuccessfully committing a "hostile action" against you as defined in rule 2.1.2.2.
+         2. For arresting or fining you.
+         3. For damaging your vehicle.
+         4. For insulting you.
+         5. For following you for an extended period of time.
+         6. For placing a hit on you.
+      6. **Leave the server while being pursued by law enforcement or by a hitman when you have a hit**.
+         1. EXCEPTIONS to rule 2.1.6:
+            1. Waiting at least thirty (30) minutes before returning to the server is not a violation of rule 2.1.6.
+      7. **Enter an instanced or other area separated from what is accessible by the general public while being chased by someone allowed to kill you per rule 2.1.1**.
+         1. EXCEPTIONS to rule 2.1.7:
+            1. Entering an area that the chaser can enter is not a violation of rule 2.1.7.
+            2. A criminal may escape law enforcement by entering the black market and is not a violation of rule 2.1.7.
+      8. **Exchange any Lsrcr goods or services for real-life currency**. These goods include, but are not limited to, donator points, jail cards, or duel cards.
+      9. **Scoreboost**. Scoreboosting is repeatedly selling items, weapons, etc, or otherwise increasing your score in a particular class by collaborating with another account or by any other means in order to increase your score at an expedited rate. Leaving to evade cooldown timers shall also be considered scoreboosting.
+      10. **Improperly use chat channels**:
+          1. All chat channels shall be used for their original intended purpose. This includes administrative channels such as /ask or /adm.
+          2. Under no circumstances shall any non-English language be spoken in main chat.
+          3. Do not make /q and/or /reconnect jokes as a way of tricking players to closing/restarting their connection to the server.
+          4. Posting links other than those from the domain "ls-rcr.com".
+      11. **Improperly utilize the water cannon**. The water cannon shall only be used when killing is allowed under rule 2.1.1.
+      12. **Kidnap or drag an AFK player**.
+      13. **Go AFK while selling hot dogs or sell hot dogs right next to checkpoints**.
+      14. **Own more than one non-donation house at a time**. This rule applies to each PERSON--meaning you may not set up multiple accounts to circumvent this rule.
+          1. NOTE: When a donation house is transferred from one player to another (not purchased directly from Lsrcr with donation points), the house automatically becomes a non-donation house.
+      15. **Trade any donation items other than donation points, jail cards, duel cards, or houses with other players**.
+          1. Players with these points are permitted to sell or trade them for any reason (within the constraints of rule 2.1.8).
+          2. Players may not directly ask others to trade unless responding to a generic public enquiry made in the game's main chat or a post on the forums. This also includes direct enquiries for free donation items.
+      16. **Scam unless all of the following apply**:
+          1. Scammer does not have the [Lsrcr] tag.
+          2. Scammer does not scam more than $300,000.
+          3. Trade does not involve any donation items. This does not include a donated house.
+          4. The victim of the scam has at least 500 score.
+      17. **Ban evade**. You may not circumvent the ban system by creating another account and/or using a different IP address from the one that is banned.
+      18. **Asking staff members to do events or for packets, teleports, etc**.
+
+3. # Lsrcr Forum-Specific Rules
+
+   1. **You or your account(s) shall not--intentionally or unintentionally--do any of the following actions**:
+      1. **Post support tickets or forum posts (like reports or unban appeals) in an attempt to expedite its review**. Admins know the reports are there, and they will get to them as soon as they can.
+      2. **Make topics or post in topics that may insight hatred or flaming**. If something is wrong, make a report or private message an administrator, assistant senior administrator, senior administrator, or the head administrator.
+      3. **Go off topic**. Go to "The Bar" if you want to be random. Being random does not include being bashful and hateful.
+      4. **Bump old topics** unless you are adding something useful or interesting to the conversation.
+
+4. # Lsrcr TeamSpeak-Specific Rules
+
+   1. **You or your account(s) shall not--intentionally or unintentionally--do any of the following actions**:
+      1. **Poke spam** other users.
+      2. **Whisper spam** other users.
+
+5. # Lsrcr Discord-Specific Rules
+
+   1. The "Lsrcr Discord" is a subdivision of the "Half-a-Nerd" Discord. 
+   2. **You or your account(s) shall not--intentionally or unintentionally--do any of the following actions**:
+      1. **Spam tag other users/groups without a good reason**. If someone was recently tagged, do not tag them again unless necessary.
+      2. **Tag "@everyone", "@here", or any other large group without good reason or justification**.
+         1. For example:
+            1. Tagging "@here" would be appropriate in the "#twitch-youtube" channel if advertising your current stream or a new YouTube video you uploaded.
+            2. Tagging "@everyone" in "#general" to say "Come in game!" would not be appropriate.
+            3. Tagging "@SAMP Staff" in "#support" to say "ID 32 ([Lsrcr]Energ) is using airbrake. There are no admins online." would be appropriate.
+
+
+6. EXCEPTIONS to rules 1, 2, 3, 4, and 5:
+
+   1. Any Lsrcr administrator ranked level 4 and above is exempt from any rule that becomes necessary to violate in order to carry out administrative duties of Lsrcr as long as that duty falls within that administrator's assigned scope of duties.
+      1. Some examples:
+         1. An administrator may need to violate rule 1.1.8 in order to resolve a support ticket issue such as changing a player's email on their behalf. This would be an appropriate usage of this exception.
+         2. If an administrator publicly posted a player's email address on the forum, this would not be an appropriate usage of this exception, and would be a flagrant violation of the server rules and players' trust.
+         3. If an administrator were to deathmatch a player to further investigate one who they determine may be utilizing cheats, this would be an appropriate usage of this exception.
+`;
